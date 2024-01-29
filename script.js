eadd5fb9c09c2818417752e3f3c3bd991b00bdec
dadada <dadada228@gmail.com>
2024-01-29 13:11:42 +0500

    base setup project

---
+++ script.js
@@ -0,0 +1,15 @@
+var yourName = prompt('Введите своё имя')
+var yourAge = prompt('Введите свой возраст')
+var primer1 = prompt('1. Решите пример 8 + 2 = ?')
+var primer2 = prompt('2. Решите пример 31 - 6 = ?')
+var primer3 = prompt('3. Решите пример 5 * 10 = ?')
+var primer4 = prompt('4. Решите пример 9 / 6 = ?')
+var primer5 = prompt('5. Решите пример 1 % 7 = ?')
+alert('Откройте консоль для просмотра (F12)')
+console.log('Меня зовут ' + yourName);
+console.log('Мне ' + yourAge + ' лет/года');
+console.log('Пример 1: 8 + 2 = 10 Ваш ответ ' + primer1);
+console.log('Пример 2: 31 - 6 = 25 Ваш ответ ' + primer2);
+console.log('Пример 3: 5 * 10 = 50 Ваш ответ ' + primer3);
+console.log('Пример 4: 9 / 6 = 1,5 Ваш ответ ' + primer4);
+console.log('Пример 5: 10 % 3 = 1 Ваш ответ ' + primer5);
